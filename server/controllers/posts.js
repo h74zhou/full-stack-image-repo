93386066e9e96fa38be0da078ad570edb9cade70
Herun Zhou <iamherunzhou@gmail.com>
2020-12-28 19:23:47 -0500

    successfully created POST request

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -15,9 +15,10 @@ export const createPost = async (req, res) => {
     const newPost = new PostMessage(post);
 
     try  {
-        await newPost().save()
-        res.status(201).json(newPost)
+        await newPost.save();
+        res.status(201).json(newPost);
     } catch (error) {
+        console.log(error);
         res.status(409).json({message: error.message})
     }
     
