61fba51eaf00a81aa18e946048e7bab2d2d5ddef
Herun Zhou <iamherunzhou@gmail.com>
2021-01-02 21:54:55 -0500

    finished implementing update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from './actions/posts';
@@ -8,35 +8,47 @@ import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
-
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
+  const [currentId, setcurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch]);    
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
-    return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image}src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
+  return (
+    <Container maxidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt='memories'
+          height='60'
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='stretch'
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setcurrentId={setcurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setcurrentId={setcurrentId} />
+            </Grid>
+          </Grid>
         </Container>
-    );
-}
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,41 +1,133 @@
-import React, { useState } from 'react';
-import {TextField, Button, Typography, Paper} from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
-    const dispatch = useDispatch();
-    const classes = useStyles();
-    const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
+// Get current ID of the post we're on
+
+const Form = ({ currentId, setcurrentId }) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
+
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((p) => p._id === currentId) : null
+  );
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+    clear();
+  };
+
+  const clear = () => {
+    setcurrentId(null);
+    setPostData({
+      creator: '',
+      title: '',
+      message: '',
+      tags: '',
+      selectedFile: '',
     });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("handleSubmit Called");
-        dispatch(createPost(postData));
-    }
-
-    const clear = () => {
-
-    }
-
-    return (
-        <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
-                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})}/></div>
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit </Button>
-                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth> Clear </Button> 
-            </form>
-        </Paper>
-    );
-}
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete='off'
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant='h6'>
+          {currentId ? 'Editing' : 'Creating'} a Memory
+        </Typography>
+        <TextField
+          name='creator'
+          variant='outlined'
+          label='Creator'
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name='title'
+          variant='outlined'
+          label='Title'
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name='message'
+          variant='outlined'
+          label='Message'
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name='tags'
+          variant='outlined'
+          label='Tags'
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type='file'
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant='contained'
+          color='primary'
+          size='large'
+          type='submit'
+          fullWidth
+        >
+          {' '}
+          Submit{' '}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='small'
+          onClick={clear}
+          fullWidth
+        >
+          {' '}
+          Clear{' '}
+        </Button>
+      </form>
+    </Paper>
+  );
+};
 export default Form;
